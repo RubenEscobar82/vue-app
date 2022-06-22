007bbfa08cf8082f209906fd54702728f7acedd7
RubenEscobar82 <60715916+RubenEscobar82@users.noreply.github.com>
2022-06-22 10:17:46 -0600

    14th update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      Edited text [v13]
+      Edited text [v14]
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
